b9ba33f879a3d92335affe15bc9cc9a3bf2e8eca
VarenytsiaV <victoriavarenytsia@gmail.com>
2024-04-26 20:26:59 +0300

    interface and functionality big update

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//Initial References
 const letterContainer = document.getElementById('letter-container')
 const optionsContainer = document.getElementById('options-container')
 const userInputSection = document.getElementById('user-input-section')
 const newGameContainer = document.getElementById('new-game-container')
 const newGameButton = document.getElementById('new-game-button')
-const canvas = document.getElementById('canvas')
 const resultText = document.getElementById('result-text')
 
 let options = {
@@ -17,40 +15,52 @@ let options = {
 		'Watermelon',
 	],
 	animals: ['Hamster', 'Horse', 'Squirrel', 'Panther', 'Rabbit', 'Zebra'],
-	colors: ['Orange','Emerald', 'Purple','Terracotta','Maroon'],
-	countries: ['India','Hungary','Italy','Switzerland','France','Ukraine',],
+	colors: ['Orange', 'Emerald', 'Purple', 'Terracotta', 'Maroon'],
+	countries: ['India', 'Hungary', 'Italy', 'Switzerland', 'France', 'Ukraine'],
 }
 
 let winCount = 0
 let count = 0
 let chosenWord = ''
 
-const displayOptions = () => {
-	optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`
+ const displayOptions = () => {
+	optionsContainer.innerHTML += `<h3>Please Select A Theme</h3>`
 	let buttonCon = document.createElement('div')
+
 	for (let value in options) {
 		buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`
 	}
+
 	optionsContainer.appendChild(buttonCon)
+
+	document.querySelectorAll('.options').forEach(button => {
+		button.addEventListener('click', function () {
+			document.querySelector('.modal-options').style.display = 'none'
+			document.querySelector('.theme').innerText = this.innerText
+			document.querySelector('.header').classList.remove('none')
+			document.querySelector('.game-container').classList.remove('none')
+		})
+	})
 }
 
 const blocker = () => {
 	let optionsButtons = document.querySelectorAll('.options')
 	let letterButtons = document.querySelectorAll('.letters')
+
 	optionsButtons.forEach(button => {
 		button.disabled = true
 	})
 
 	letterButtons.forEach(button => {
-		button.disabled.true
+		button.disabled = true
 	})
+
 	newGameContainer.classList.remove('hide')
 }
 
-//Word Generator
 const generateWord = optionValue => {
 	let optionsButtons = document.querySelectorAll('.options')
-	//If optionValur matches the button innerText then highlight the button
+
 	optionsButtons.forEach(button => {
 		if (button.innerText.toLowerCase() === optionValue) {
 			button.classList.add('active')
@@ -58,79 +68,88 @@ const generateWord = optionValue => {
 		button.disabled = true
 	})
 
-	//initially hide letters, clear previous word
+	document.querySelector('.theme').innerText = optionValue
+
 	letterContainer.classList.remove('hide')
 	userInputSection.innerText = ''
 
 	let optionArray = options[optionValue]
-	//choose random word
 	chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)]
 	chosenWord = chosenWord.toUpperCase()
 
-	//replace every letter with span containing dash
-	let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>')
+	let displayItem = chosenWord.replace(
+		/./g,
+		'<span class="letters-not-chosen dashes">_</span>'
+	)
 
-	//Display each element as span
 	userInputSection.innerHTML = displayItem
 }
 
-//Initial Function (Called when page loads/user presses new game)
 const initializer = () => {
 	winCount = 0
 	count = 0
 
-	//Initially erase all content and hide letteres and new game button
 	userInputSection.innerHTML = ''
 	optionsContainer.innerHTML = ''
 	letterContainer.classList.add('hide')
 	newGameContainer.classList.add('hide')
+	document.querySelector('.modal-game').classList.add('hide')
 	letterContainer.innerHTML = ''
 
-	//For creating letter buttons
 	for (let i = 65; i < 91; i++) {
 		let button = document.createElement('button')
 		button.classList.add('letters')
-		//Number to ASCII[A-Z]
 		button.innerText = String.fromCharCode(i)
-		//character button click
 		button.addEventListener('click', () => {
 			let charArray = chosenWord.split('')
 			let dashes = document.getElementsByClassName('dashes')
-			//if array contains clciked value replace the matched dash with letter else dram on canvas
+			let letters = document.querySelectorAll('.letters')
+			button.classList.add('chosen-letter')
+
 			if (charArray.includes(button.innerText)) {
 				charArray.forEach((char, index) => {
-					//if character in array is same as clicked button
 					if (char === button.innerText) {
-						//replace dash with letter
 						dashes[index].innerText = char
-						//increment counter
 						winCount += 1
-						//if winCount equals word lenfth
+						dashes[index].classList.remove('letters-not-chosen')
 						if (winCount == charArray.length) {
+							document.querySelector('.header').classList.add('none')
+							document.querySelector('.game-container').classList.add('none')
 							resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`
-							//block all buttons
+							document.querySelector('.modal-game').classList.remove('none')
 							blocker()
 						}
 					}
 				})
 			} else {
 				count += 1
-				document.querySelector('.count-span').innerHTML=count
-				if (count == 6) {
+				document.querySelector('.count-span').innerHTML = 9 - count
+				if (count == 9) {
+					document.querySelector('.header').classList.add('none')
+					document.querySelector('.game-container').classList.add('none')
 					resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`
+					document.querySelector('.modal-game').classList.remove('none')
 					blocker()
 				}
 			}
-			//disable clicked button
 			button.disabled = true
 		})
 		letterContainer.append(button)
 	}
 
 	displayOptions()
-	//Call to canvasCreator (for clearing previous canvas and creating initial canvas)
-	//initialDrawing would draw the frame
 }
 
-newGameButton.addEventListener('click', initializer)
+
+const startNewGame = () => {
+	console.log('lose')
+	location.reload() 
+
+	initializer()
+}
+
+
+
+//newGameButton.addEventListener('click', initializer)
 window.onload = initializer
+
